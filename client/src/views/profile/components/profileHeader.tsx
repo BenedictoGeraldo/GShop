8cb3fc5c862fb760f78da023d18fd1ca1c2f1ad7
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-03 01:23:39 +0700

    [add] profile component header

---
+++ client/src/views/profile/components/profileHeader.tsx
@@ -0,0 +1,12 @@
+const ProfileHeader = () => {
+  return (
+    <div className="header flex flex-col items-center">
+      <h1 className="text-lg sm:text-xl md:text-2xl">Profil Saya</h1>
+      <h3 className="text-sm sm:text-base md:text-lg text-center text-gray-700">
+        Kelola Informasi Seputar Profil Anda
+      </h3>
+    </div>
+  );
+};
+
+export default ProfileHeader;
