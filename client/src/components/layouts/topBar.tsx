ed8fbfd57c83cc17febf541b818ba39fd53f6d94
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-05 01:21:41 +0700

    [update] topbar

--- client/src/components/layouts/topBar.tsx
+++ client/src/components/layouts/topBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { RiSendPlaneLine } from "@remixicon/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -15,28 +16,34 @@ const TopBar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-blue-300 h-15 flex justify-between">
-      <div>
+    <div className="fixed bg-white border-b border-black/20 w-full h-16 flex justify-between">
+      <div className="mx-5 my-2">
         <Image
           src="/gshop-logo.png"
           alt="Logo GShop"
           width={80}
-          height={99}
+          height={1}
           onClick={handleLogoClick}
         />
       </div>
 
-      <div className="flex">
-        <button className="border-1" onClick={handleOrderClick}>
+      <div className="flex my-3 mx-5">
+        <button
+          className=" text-[#2461B3] text-xs justify-center items-center font-medium flex mx-4 bg-blue-200 hover:bg-blue-300 px-2 rounded-2xl"
+          onClick={handleOrderClick}
+        >
+          <i className="ri-send-plane-line text-[#2461B3]">
+            <RiSendPlaneLine />
+          </i>
           Order Now
         </button>
 
         <Image
-          className="border-1 rounded-full"
+          className="border-1 rounded-full mx-1"
           src="/profile.jpeg"
           alt="Your profile"
-          width={30}
-          height={30}
+          width={40}
+          height={40}
         />
       </div>
     </div>
