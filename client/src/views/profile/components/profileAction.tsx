a7d1f78324fa0389650166ecc2cc5a25182110dc
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-05 01:23:39 +0700

    [update] profile action components

--- client/src/views/profile/components/profileAction.tsx
+++ client/src/views/profile/components/profileAction.tsx
@@ -1,3 +1,5 @@
+import { RiSave2Line } from "@remixicon/react";
+
 interface ProfileActionsProps {
   isLoading: boolean;
   onBackClick: () => void;
@@ -12,19 +14,21 @@ const ProfileActions = ({
   return (
     <div className="flex justify-between">
       <button
-        className="back border-2 text-blue-500 rounded-md px-3"
+        className="back border-2 text-blue-500 rounded-lg w-25 h-8"
         onClick={onBackClick}
         type="button"
       >
         Kembali
       </button>
       <button
-        className="save border-2 text-blue-500 rounded-md px-3"
+        className="save flex justify-center items-center text-center bg-blue-700 hover:bg-blue-900 text-white rounded-lg w-25 h-8"
         type="button"
         disabled={isLoading}
         onClick={onSaveClick}
       >
         {isLoading ? "Menyimpan..." : "Simpan"}
+        <i className="ri-save-2-line" />
+        <RiSave2Line size={20} color="rgba(255,255,255,1)" />
       </button>
     </div>
   );
